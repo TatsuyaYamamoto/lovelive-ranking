b43b4560e53aaf1adf3d2bb061509a4c4477faf0
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2015-12-04 09:24:32 +0900

    TODO http通信クラスのテスト用簡易webサーバーの構築

--- src/test/java/net/sokontokoro_factory/lib/twitter/SPConnectionTest.java
+++ src/test/java/net/sokontokoro_factory/lib/twitter/SPConnectionTest.java
@@ -2,14 +2,19 @@ package net.sokontokoro_factory.lib.twitter;
 
 import static org.junit.Assert.*;
 
+import java.util.UUID;
+
 import org.junit.Test;
 
 public class SPConnectionTest {
 
-	@Test
+	// @Test
 	public void post通信出来る() {
 		String endpoint = "https://api.twitter.com/oauth/request_token";
-		String header = "OAuth oauth_consumer_key=DqMQV83OkYYNfhiepou491bAV, oauth_nonce=40f52f9777c263b892bd164cf784d445, oauth_signature=%2Bu3oN56g84njqKOuPqkftefdeKY%3D, oauth_signature_method=HMAC-SHA1, oauth_timestamp=1449072274, oauth_version=1.0";
+		String timestamp = Long.toString(System.currentTimeMillis() / 1000);
+		String oauth_nonce = UUID.randomUUID().toString();
+		
+		String header = "OAuth oauth_consumer_key=DqMQV83OkYYNfhiepou491bAV, oauth_nonce=" + oauth_nonce + ", oauth_signature=%2Bu3oN56g84njqKOuPqkftefdeKY%3D, oauth_signature_method=HMAC-SHA1, oauth_timestamp=" + timestamp + ", oauth_version=1.0";
 		SPConnection connection = new SPConnection(endpoint, header, "");
 		String responseBody = connection.post();
 		assertTrue(responseBody.contains("oauth_token"));
