a7d638d33a06040f760f0588ddd08de6084dc940
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2016-01-07 00:09:55 +0900

    travis-CI用db構築スクリプトを追加

---
+++ .testDB.sql
@@ -0,0 +1,22 @@
+drop database if exists sokontokoro_game_test;
+
+create database sokontokoro_game_test character set utf8;
+
+CREATE TABLE sokontokoro_game_test.score (
+	game_name CHAR(10) NOT NULL, 
+	user_id int NOT NULL, 
+	point int NOT NULL, 
+	create_date DATETIME NOT NULL, 
+	update_date DATETIME NOT NULL, 
+	final_date DATETIME NOT NULL, 
+	count int NOT NULL, 
+	PRIMARY KEY(game_name, user_id));
+
+
+CREATE TABLE sokontokoro_game_test.user (
+	id int NOT NULL KEY, 
+	name CHAR(16) NOT NULL, 
+	create_date DATETIME NOT NULL, 
+	update_date DATETIME NULL, 
+	deleted BOOLEAN NULL DEFAULT false, 
+	admin BOOLEAN NULL DEFAULT false);
