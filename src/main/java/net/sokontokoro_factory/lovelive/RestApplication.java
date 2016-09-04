1add041c360e9875c99577e5898f126473f08706
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2016-09-04 19:29:18 +0900

    register excption mapping classes

--- src/main/java/net/sokontokoro_factory/lovelive/RestApplication.java
+++ src/main/java/net/sokontokoro_factory/lovelive/RestApplication.java
@@ -1,5 +1,7 @@
 package net.sokontokoro_factory.lovelive;
 
+import net.sokontokoro_factory.lovelive.controller.errorhandling.NoResourceExceptionMapper;
+import net.sokontokoro_factory.lovelive.exception.InvalidArgumentException;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
@@ -8,7 +10,10 @@ import javax.ws.rs.ApplicationPath;
 public class RestApplication extends ResourceConfig {
     public RestApplication() {
         // resource classes
-        packages(false, this.getClass().getPackage().getName() + ".controller.resource");
+        packages(false, this.getClass().getPackage().getName() + ".controller.resource")
+                .register(NoResourceExceptionMapper.class)
+                .register(InvalidArgumentException.class);
+
         // filter classes
         packages(false, this.getClass().getPackage().getName() + ".filter");
     }
