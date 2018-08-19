119a0a03fcf6f62ce67cd580bdb8d9b48cb66b7e
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-08-19 18:15:29 +0900

    set CORS policy config

--- src/main/java/net/sokontokoro_factory/lovelive/Application.java
+++ src/main/java/net/sokontokoro_factory/lovelive/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -15,5 +19,23 @@ public class Application extends SpringBootServletInitializer {
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(Application.class);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins(
+                "http://localhost",
+                "http://games.sokontokoro-factory.net",
+                "https://games.sokontokoro-factory.net")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
   }
 }
